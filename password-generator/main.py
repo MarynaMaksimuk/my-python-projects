8ee82369a6f745c9d26bfdc1865a2d910d2f300e
Maryna Maksimuk <maksimarina@gmail.com>
2024-03-27 16:10:21 -0500

    Update of Password Generator project- tkinter, json

--- password-generator/main.py
+++ password-generator/main.py
@@ -1,12 +1,17 @@
 from tkinter import *
 from tkinter import messagebox
-from  random import choice, shuffle, randint
+from random import choice, shuffle, randint
 import pyperclip
+import json
+
+
 # --------------------------------PASSWORD GENERATOR------------------------
-#Password Generator Project
+# Password Generator Project
 
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -16,25 +21,39 @@ def generate_password():
 
     password_list = password_letters + password_numbers + password_char
     shuffle(password_list)
-    print(password_list)
     password = "".join(password_list)
 
     # print(f"Your password is: {password}")
     password_entry.insert(0, password)
     pyperclip.copy(password)
+
+
 # ----------------------------------SAVE PASSWORD---------------------------
 def save():
     website = website_entry.get()
     password = password_entry.get()
     email = email_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave required fields empty")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {email}"
-                                                      f"\nPassword: {password}\nIs it ok?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website}   |   {email}   |   {password}\n")
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
@@ -79,4 +98,3 @@ add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
-
